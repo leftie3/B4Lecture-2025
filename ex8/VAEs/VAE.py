8f85b9d6771aaf4195ea4e26fa1115522b94ef92
leftie3 <kaselias@yahoo.gr>
2025-06-17 15:55:32 +0900

    partial implementation

--- ex8/VAEs/VAE.py
+++ ex8/VAEs/VAE.py
@@ -2,6 +2,7 @@
 """This file is for you to implement VAE. Add variables as needed."""
 
 import numpy as np
+import torch
 import torch.nn as nn
 
 MNIST_SIZE = 28
@@ -23,29 +24,88 @@ class VAE(nn.Module):
             Dropout rate.
         """
         super(VAE, self).__init__()
-        self.eps = np.spacing(1)
-        self.x_dim = MNIST_SIZE * MNIST_SIZE  # The image in MNIST is 28×28
+        self.eps = np.spacing(1)  # Tiny number epsilon
+        self.x_dim = MNIST_SIZE * MNIST_SIZE  # The image in MNIST is
+
+        # Constructor parameters
         self.z_dim = z_dim
-        self.h_dim = h_dim
+        self.h_dim = h_dim  # Default 400
         self.drop_rate = drop_rate
 
-        self.enc_fc1 = nn.Linear(self.x_dim, self.h_dim)
-        self.enc_fc2 = nn.Linear(self.h_dim, int(self.h_dim / 2))
+        # Model layers
+
+        # Encoding
+        self.enc_layer1 = nn.Linear(self.x_dim, self.h_dim)
+        self.enc_layer2 = nn.Linear(self.h_dim, int(self.h_dim / 2))
         self.enc_fc3_mean = nn.Linear(int(self.h_dim / 2), z_dim)
         self.enc_fc3_logvar = nn.Linear(int(self.h_dim / 2), z_dim)
-        self.dec_fc1 = nn.Linear(z_dim, int(self.h_dim / 2))
-        self.dec_fc2 = nn.Linear(int(self.h_dim / 2), self.h_dim)
+
+        # Decoding
+        self.dec_layer1 = nn.Linear(z_dim, int(self.h_dim / 2))
+        self.dec_layer2 = nn.Linear(int(self.h_dim / 2), self.h_dim)
         self.dec_drop = nn.Dropout(self.drop_rate)
-        self.dec_fc3 = nn.Linear(self.h_dim, self.x_dim)
+        self.dec_layer3 = nn.Linear(self.h_dim, self.x_dim)
 
-    def encoder(self):
-        """# ToDo: Implement the encoder."""
+    def encoder(self, x):
+        """Run data through the encoder model.
 
-    def sample_z(self):
-        """# ToDo: Implement a function to sample latent variables."""
+        Args:
+            x (torch.Tensor): Input data tensor.
+        Returns:
+            (torch.Tensor, torch.Tensor): The mean and log variance tensors
+        """
+        # Apply encoding layers with ReLU
+        x = self.enc_layer1(x)
+        x = nn.functional.relu(x)
 
-    def decoder(self):
-        """# ToDo: Implement the decoder."""
+        x = self.enc_layer2(x)
+        x = nn.functional.relu(x)
+
+        # Compress into latent space
+        mean = self.enc_fc3_mean(x)
+        log_var = self.enc_fc3_logvar(x)
+
+        return mean, log_var
+
+    def sample_z(self, mean: torch.Tensor, log_var: torch.Tensor):
+        """Retrieve sample from latent space.
+
+        Args:
+            mean (torch.Tensor):
+            log_var (torch.Tensor):
+        Returns:
+            torch.Tensor: The generated sample.
+        """
+        input_shape = mean.shape  # (batch, dim)
+
+        # Sample from standard normal distribution
+        sample_norm = torch.randn(input_shape)
+
+        # Fit sample to be of the input distribution
+        return mean + torch.exp(log_var * 0.5) * sample_norm
+
+    def decoder(self, z):
+        """Reconstruct data from the latent sample.
+
+        Args:
+            z (torch.Tensor): The retrieved sample of the latent distribution.
+        Returns:
+            torch.Tensor: The decoded data tensor.
+        """
+        # Apply decoding layers with ReLU
+        z = self.dec_layer1(z)
+        z = nn.functional.relu(z)
+
+        z = self.dec_layer2(z)
+        z = nn.functional.relu(z)
+
+        z = self.dec_drop(z)
+
+        # Use sigmoid for output layer
+        z = self.dec_layer3(z)
+        z = nn.functional.sigmoid(z)
+
+        return z
 
     def forward(self):
         """# ToDo: Implement the forward function to return the following variables."""
